31345d11f20810affcca9714bacac35380d56b64
shahan <shahan@gmail.com>
2022-08-22 23:24:13 +0500

    add parametrize to test_bank_transaction and raise exception if amount less than withdrawal

--- app/calculations.py
+++ app/calculations.py
@@ -22,6 +22,8 @@ class BankAccount:
         self.balance += amount
 
     def withdraw(self, amount):
+        if amount > self.balance:
+            raise Exception("Insufficient funds in account")
         self.balance -= amount
 
     def collect_interest(self):
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -4,6 +4,7 @@ from app.calculations import add, subtract, multiply, divide, BankAccount
 
 @pytest.fixture
 def zero_bank_account():
+    print("creating empty bank account")
     return BankAccount()
 
 
@@ -37,22 +38,29 @@ def test_bank_set_initial_amount(bank_account):
 
 
 def test_bank_default_amount(zero_bank_account):
-    assert bank_account.balance == 0
+    print("testing my bank account")
+    assert zero_bank_account.balance == 0
 
 
-def test_bank_withdraw():
-    bank_account = BankAccount(100)
+def test_bank_withdraw(bank_account):
     bank_account.withdraw(20)
     assert bank_account.balance == 80
 
 
-def test_bank_deposit():
-    bank_account = BankAccount(100)
+def test_bank_deposit(bank_account):
     bank_account.deposit(20)
     assert bank_account.balance == 120
 
 
-def test_collect_interest():
-    bank_account = BankAccount(100)
+def test_collect_interest(bank_account):
     bank_account.collect_interest()
     assert bank_account.balance == 100 * 1.1
+
+
+@pytest.mark.parametrize(
+    "deposited, withdrew, expected", [(200, 100, 100), (50, 30, 20), (1200, 200, 1000)]
+)
+def test_bank_transaction(zero_bank_account, deposited, withdrew, expected):
+    zero_bank_account.deposit(deposited)
+    zero_bank_account.withdraw(withdrew)
+    assert zero_bank_account.balance == expected
