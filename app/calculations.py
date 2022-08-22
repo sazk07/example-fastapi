de4ca52dc8273453eb7d0c55983d24ff50bba6c8
shahan <shahan@gmail.com>
2022-08-22 22:33:02 +0500

    add class to calculations.py

--- app/calculations.py
+++ app/calculations.py
@@ -12,3 +12,17 @@ def multiply(num_1: int, num_2: int):
 
 def divide(num_1: int, num_2: int):
     return num_1 / num_2
+
+
+class BankAccount:
+    def __init__(self, starting_balance=0):
+        self.balance = starting_balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    def collect_interest(self):
+        self.balance *= 1.1
