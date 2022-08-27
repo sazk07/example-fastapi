d4cf6e9f86862728e8aa2a7c605a5ad4c121a4a6
shahan <shahan@gmail.com>
2022-08-27 21:45:31 +0500

    change test_update_other_user_post where authorized client will attempt to change post not theirs

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -135,8 +135,12 @@ def test_update_other_user_post(test_user, authorized_client, test_posts):
     data = {
         "title": "updated title",
         "posts_content": "updated content",
-        "posts_id": test_posts[0].posts_id,
+        "posts_id": test_posts[3].posts_id,
     }
     response = authorized_client.put(f"/posts/{test_posts[3].posts_id}", json=data)
-    print(response.json())
     assert response.status_code == 403
+
+def test_unauthorized_user_update_post(client,test_user,test_posts):
+    """test to check if unauthorized user is forbidden from updating posts"""
+    response = client.put(f"/posts/{test_posts[0].posts_id}")
+    assert response.status_code == 401
